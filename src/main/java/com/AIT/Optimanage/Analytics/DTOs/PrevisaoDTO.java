054bd194d0dd1010dec042d8a1eb478fa4bd66e1
Fernando FR <ferlicio@gmail.com>
2025-08-26 10:14:01 -0300

    chore: document analytics forecasting

---
+++ src/main/java/com/AIT/Optimanage/Analytics/DTOs/PrevisaoDTO.java
@@ -0,0 +1,13 @@
+package com.AIT.Optimanage.Analytics.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrevisaoDTO {
+    private double valorPrevisto;
+}
+
