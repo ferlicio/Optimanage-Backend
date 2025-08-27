6d015a0f373ef816d988b402e324a003d940a271
Fernando FR <ferlicio@gmail.com>
2025-08-27 17:17:27 -0300

    fix: map Fornecedor id with custom method

--- src/main/java/com/AIT/Optimanage/Mappers/ServicoMapper.java
+++ src/main/java/com/AIT/Optimanage/Mappers/ServicoMapper.java
@@ -2,16 +2,17 @@ package com.AIT.Optimanage.Mappers;
 
 import com.AIT.Optimanage.Controllers.dto.ServicoRequest;
 import com.AIT.Optimanage.Controllers.dto.ServicoResponse;
+import com.AIT.Optimanage.Models.Fornecedor.Fornecedor;
 import com.AIT.Optimanage.Models.Servico;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ServicoMapper {
 
-    @Mapping(target = "fornecedor.id", source = "fornecedorId")
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "fornecedor", source = "fornecedorId", qualifiedByName = "idToFornecedor")
     @Mapping(target = "ownerUser", ignore = true)
     Servico toEntity(ServicoRequest request);
 
@@ -21,5 +22,15 @@ public interface ServicoMapper {
 
     @Mapping(target = "fornecedorId", source = "fornecedor.id")
     ServicoRequest toRequest(Servico servico);
+
+    @Named("idToFornecedor")
+    default Fornecedor mapFornecedor(Integer fornecedorId) {
+        if (fornecedorId == null) {
+            return null;
+        }
+        Fornecedor fornecedor = new Fornecedor();
+        fornecedor.setId(fornecedorId);
+        return fornecedor;
+    }
 }
 
