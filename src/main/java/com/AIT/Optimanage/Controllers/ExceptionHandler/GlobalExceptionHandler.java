3414c46c940703373ca19d6d31fb8ee69c37299f
Fernando FR <ferlicio@gmail.com>
2025-08-26 23:39:58 -0300

    correcao de importações e dependencia para globalExceptionHandler

--- src/main/java/com/AIT/Optimanage/Controllers/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/AIT/Optimanage/Controllers/ExceptionHandler/GlobalExceptionHandler.java
@@ -6,10 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.validation.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.validation.DefaultMessageSourceResolvable;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 
 import com.AIT.Optimanage.Exceptions.CustomRuntimeException;
 
