9ca78a4aecfe029aa91e9d98156c6789c56e3b21
Fernando FR <ferlicio@gmail.com>
2025-08-27 11:39:15 -0300

    novo metodo de rotação de chaves jwt

--- src/main/java/com/AIT/Optimanage/Config/JwtProperties.java
+++ src/main/java/com/AIT/Optimanage/Config/JwtProperties.java
@@ -1,12 +1,10 @@
 package com.AIT.Optimanage.Config;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
-@Setter
 @Component
 @ConfigurationProperties(prefix = "app.jwt")
 public class JwtProperties {
@@ -17,9 +15,23 @@ public class JwtProperties {
     private long refreshExpiration;
 
     /**
-     * Switch the primary key to the rotation key.
+     * Configure a new rotation key that will become the primary key after the
+     * next scheduled rotation.
+     *
+     * @param newRotationKey the key to use for the next rotation
+     */
+    public void configureRotationKey(String newRotationKey) {
+        this.rotationKey = newRotationKey;
+    }
+
+    /**
+     * Swap the current primary and rotation keys. The previous primary key
+     * becomes the new rotation key so it can be reused if necessary.
      */
     public void switchKeys() {
+        String previousPrimary = this.primaryKey;
         this.primaryKey = this.rotationKey;
+        this.rotationKey = previousPrimary;
     }
+    
 }
