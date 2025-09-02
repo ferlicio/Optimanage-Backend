0438e2c153241c1e24defe64395ec69b27b953b1
Fernando FR <ferlicio@gmail.com>
2025-09-02 09:56:26 -0300

    Expose recommendations endpoint

---
+++ src/main/java/com/AIT/Optimanage/Controllers/RecommendationController.java
@@ -0,0 +1,35 @@
+package com.AIT.Optimanage.Controllers;
+
+import com.AIT.Optimanage.Controllers.BaseController.V1BaseController;
+import com.AIT.Optimanage.Controllers.dto.ProdutoResponse;
+import com.AIT.Optimanage.Models.User.User;
+import com.AIT.Optimanage.Services.RecommendationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recommendations")
+@RequiredArgsConstructor
+@Tag(name = "Recomendações", description = "Sugestões de produtos baseadas em vendas")
+public class RecommendationController extends V1BaseController {
+
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/{clienteId}")
+    @Operation(summary = "Recomendar produtos", description = "Sugere produtos com base no histórico de vendas")
+    @ApiResponse(responseCode = "200", description = "Sucesso")
+    public ResponseEntity<List<ProdutoResponse>> recomendar(@AuthenticationPrincipal User loggedUser,
+                                                            @PathVariable Integer clienteId) {
+        return ok(recommendationService.recomendarProdutos(loggedUser, clienteId));
+    }
+}
