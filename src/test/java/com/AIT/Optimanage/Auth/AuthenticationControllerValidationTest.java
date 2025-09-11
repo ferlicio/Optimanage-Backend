1b14d0ea053a11ae78911420964c8aa40b9f3a7d
Fernando FR <ferlicio@gmail.com>
2025-09-11 11:26:17 -0300

    test: seed correlation id in MVC tests

--- src/test/java/com/AIT/Optimanage/Auth/AuthenticationControllerValidationTest.java
+++ src/test/java/com/AIT/Optimanage/Auth/AuthenticationControllerValidationTest.java
@@ -1,6 +1,8 @@
 package com.AIT.Optimanage.Auth;
 
 import com.AIT.Optimanage.Exceptions.GlobalExceptionHandler;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.slf4j.MDC;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -25,6 +28,16 @@ class AuthenticationControllerValidationTest {
 
     @MockBean
     private AuthenticationService authenticationService;
+
+    @BeforeEach
+    void setup() {
+        MDC.put("correlationId", "test-correlation-id");
+    }
+
+    @AfterEach
+    void cleanup() {
+        MDC.remove("correlationId");
+    }
 
     @Test
     void whenRegisterRequestInvalid_thenReturnsBadRequest() throws Exception {
