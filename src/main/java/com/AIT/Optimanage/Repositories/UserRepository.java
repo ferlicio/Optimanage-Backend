886da64a34fbd4415fdb68683c557bc69e8adc90
Fernando FR <ferlicio@gmail.com>
2025-09-19 16:59:47 -0300

    Restrict user service operations to tenant scope

--- src/main/java/com/AIT/Optimanage/Repositories/UserRepository.java
+++ src/main/java/com/AIT/Optimanage/Repositories/UserRepository.java
@@ -1,19 +1,25 @@
 package com.AIT.Optimanage.Repositories;
 
 import com.AIT.Optimanage.Models.User.User;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public Optional<User> findByEmail(String email);
 
     long countByOrganizationIdAndAtivoTrue(Integer organizationId);
+
+    Page<User> findAllByOrganizationId(Integer organizationId, Pageable pageable);
+
+    Optional<User> findByIdAndOrganizationId(Integer id, Integer organizationId);
 
     @Modifying(clearAutomatically = true)
     @Query("update User u set u.organizationId = :organizationId where u.id = :userId")
